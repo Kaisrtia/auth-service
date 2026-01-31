61d567dee8f6f220ac8acfca63616344a512b5a5
kaisrtia <nqhtrung304@gmail.com>
2026-02-01 00:00:17 +0700

    remove all test api file, make a clean main branch

--- src/main/java/com/kaisrtia/auth_service/DTO/UserDTO.java
+++ src/main/java/com/kaisrtia/auth_service/DTO/UserDTO.java
@@ -1,35 +1,30 @@
-package com.kaisrtia.auth_service.entity;
+package com.kaisrtia.auth_service.DTO;
+
+import com.kaisrtia.auth_service.Enum.Role;
+import com.kaisrtia.auth_service.entity.User;
 
 import java.util.Objects;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-
-import jakarta.validation.constraints.*;
-import org.hibernate.validator.constraints.Length;
-
-import com.kaisrtia.auth_service.Enum.Role;
-
-@Entity
-public class User {
-  private @Id @GeneratedValue long id;
-  @NotEmpty(message = "Name must not be empty")
+public class UserDTO {
+  private long id;
   private String name;
-  @NotEmpty(message = "Username must not be empty!")
   private String username;
-  @NotEmpty(message = "Password must not be empty!")
-  @Length(min = 5, message = "Password must be at least 5 characters!")
-  private String password;
   private Role role;
 
-  public User() {
+  public UserDTO() {
   }
 
-  public User(String name, String username, String password, Role role) {
+  public UserDTO(User u) {
+    this.id = u.getId();
+    this.name = u.getName();
+    this.username = u.getUsername();
+    this.role = u.getRole();
+  }
+
+  public UserDTO(long id, String name, String username, Role role) {
+    this.id = id;
     this.name = name;
     this.username = username;
-    this.password = password;
     this.role = role;
   }
 
@@ -43,10 +38,6 @@ public class User {
 
   public String getUsername() {
     return username;
-  }
-
-  public String getPassword() {
-    return password;
   }
 
   public Role getRole() {
@@ -65,10 +56,6 @@ public class User {
     this.username = username;
   }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
   public void setRole(Role role) {
     this.role = role;
   }
@@ -79,25 +66,23 @@ public class User {
       return true;
     if (o == null || getClass() != o.getClass())
       return false;
-    User user = (User) o;
+    UserDTO user = (UserDTO) o;
     return id == user.id && Objects.equals(name, user.name)
         && Objects.equals(username, user.username)
-        && Objects.equals(password, user.password)
         && Objects.equals(role, user.role);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name, username, password, role);
+    return Objects.hash(id, name, username, role);
   }
 
   @Override
   public String toString() {
-    return "User{" +
+    return "UserDTO{" +
         "id=" + id +
         ", name='" + name + '\'' +
         ", username='" + username + '\'' +
-        ", password='" + password + '\'' +
         ", role='" + role + '\'' +
         '}';
   }
---
+++ src/main/java/com/kaisrtia/auth_service/Enum/Role.java
@@ -0,0 +1,9 @@
+package com.kaisrtia.auth_service.Enum;
+
+public enum Role {
+  ADMIN,
+  HOST,
+  STUDENT,
+  GUEST
+}
+
